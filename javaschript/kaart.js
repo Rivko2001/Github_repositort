283eef1ad9af9f430305238205563f86e712e3a4
Rivko2001 <Rivkoks@hotmail.com>
2026-03-05 11:58:34 +0100

    kaart toegevoecht
    heb eenleflet kart er bij toe gevoecht

---
+++ javaschript/kaart.js
@@ -0,0 +1,75 @@
+const osm = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png');
+
+const map = L.map('map', {
+  center: [43.1050907,12.3586074],
+  zoom: 6,
+  layers: [osm]
+});
+
+const fontAwesomeIcon = L.divIcon({
+  html: '<i class="fa-solid fa-archway fa-2x"></i>',
+  iconSize: [20, 20],
+  className: 'fontawesome_icon'
+});
+
+let geojson_marker = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "naam": "milaan",
+        "adres": "Arboretum West 98, Almere"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [9.1671174,45.4684217]
+      }
+    },
+        {
+      "type": "Feature",
+      "properties": {
+        "naam": "Hogeschool Aeres",
+        "adres": "Arboretum West 98, Almere"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [8.9839714,44.3964843]
+      }
+    },
+         {
+      "type": "Feature",
+      "properties": {
+        "naam": "Hogeschool Aeres",
+        "adres": "Arboretum West 98, Almere"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [14.3667053,40.7893608]
+      }
+    },
+           {
+      "type": "Feature",
+      "properties": {
+        "naam": "Hogeschool Aeres",
+        "adres": "Arboretum West 98, Almere"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.2451375,43.7679751]
+      }
+    }
+
+  ]
+};
+
+let jsonlayer = L.geoJson(geojson_marker, {
+  onEachFeature: onEachFeature,
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: fontAwesomeIcon });
+  }
+}).addTo(map);
+
+function onEachFeature(feature, layer) {
+  layer.bindPopup("<b>" + feature.properties.naam + "</b>");
+}
